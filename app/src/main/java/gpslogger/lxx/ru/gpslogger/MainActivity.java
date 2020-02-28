82606ea2cc0008fa9ca5a0f9f63c07c8fe034ee0
lxxxxl <lxxxxl>
2020-02-28 22:24:11 +0300

    NMEA retrieval

--- app/src/main/java/gpslogger/lxx/ru/gpslogger/MainActivity.java
+++ app/src/main/java/gpslogger/lxx/ru/gpslogger/MainActivity.java
@@ -1,13 +1,68 @@
-package gpslogger.lxx.ru.gpslogger;
+package lxx.ru.gpslogger;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //NMEA listener
+        LocationManager LM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        ((LocationManager) getSystemService(Context.LOCATION_SERVICE)).requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                Log.i(
+                        "gpslogger",
+                        loc.toString()
+                );
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+        });
+
+        LM.addNmeaListener(new GpsStatus.NmeaListener() {
+            public void onNmeaReceived(long timestamp, String nmea) {
+                Log.i(
+                        "gpslogger",
+                        nmea
+                );
+
+            }});
     }
 }
